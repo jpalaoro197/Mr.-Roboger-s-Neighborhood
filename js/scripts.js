8f793cd94c89ffa44b9edb38bc694a8b3899d51e
jacobpalaoro <jpalaoro197@gmail.com>
2022-06-10 11:36:56 -0700

    beepBoop function returns a list counting up to imput number

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 function beepBoop(inputNumber) {
   let outputNumber = [];
-  for (let i = 0; i <= inputNumber; i+=1) {
-    output = [output + i];
+  for (let i = 0; i <= inputNumber; i +=1) {
+    outputNumber.push(i);
   }
+  return outputNumber;
 }
