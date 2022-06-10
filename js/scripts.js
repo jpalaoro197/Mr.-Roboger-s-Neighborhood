f020466208e4d8ee5ed2656c52facb741ddf134e
jacobpalaoro <jpalaoro197@gmail.com>
2022-06-10 15:15:31 -0700

    the beepBoop function will exchange any number including 3, 2, 1 with the corresponding text

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,11 @@ function beepBoop(inputNumber) {
   for (let i1 = 0; i1 <= inputNumber; i1 +=1) {
     outputString = i1.toString();
     for (let i2 = 3; i2 > 0; i2--) {
-      
+      if(outputString.includes(i2.toString())) {
+        outputNumber.push(roboTalk[i2-1]);
+      }
     }
-    
+    if (outputNumber.length < (i1 + 1)) { outputNumber.push(i1); }
     }
-  }
   return outputNumber;
 }
