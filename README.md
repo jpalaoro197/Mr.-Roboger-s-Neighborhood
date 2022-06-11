d43917165be3c68d5f9b2d6aa593cf0dabc855a5
jacobpalaoro <jpalaoro197@gmail.com>
2022-06-10 18:00:34 -0700

    remove most buisness logic, it was over complicated and did not work.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+Describe: beepBoop()
+
+Test: "It should return an array of numbers from 0 to the user's inputted number"
+Code: beepBoop(5);
+Expected Output: [0, 1, 2, 3, 4, 5]
+
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,9 @@
 function beepBoop(inputNumber) {
-  let roboTalk = ["beep", "boop", "wont you be my neighbor?"]
   let outputNumber = [];
-  let outputString;
   for (let i1 = 0; i1 <= inputNumber; i1 +=1) {
-    outputString = i1.toString();
-    for (let i2 = 3; i2 > 0; i2--) {
-      if(outputString.includes(i2.toString())) {
-        outputNumber.push(roboTalk[i2-1]);
-      }
-    }
-    if (outputNumber.length < (i1 + 1)) { outputNumber.push(i1); }
-    }
-  return outputNumber;
-}
+
+  }
+
 
 $(document).ready(function(){
   $("form#talk").submit(function(){
